a71a6d301229150b9bd02feb13fb9deddc0ba755
Muhamad Islahuddin <el.islah.udin@gmail.com>
2022-07-20 14:22:37 +0700

    fix token

--- pages/api/loginnext.js
+++ pages/api/loginnext.js
@@ -3,7 +3,7 @@ export default function loginnext(req, res) {
   if (req.method == "POST"){
     console.log('add token to cookie')
     const token = req.body.token
-    res.setHeader('Set-Cookie', `token=${token} ; Domain=.herokuapp.com ; Path=/ ; httpOnly ; Max-Age : 9000000000; Secure ; SameSite=None`);
+    res.setHeader('Set-Cookie', `token=${token} ; Domain='.herokuapp.com' ; Path=/ ; httpOnly ; Max-Age : 9000000000; Secure ; SameSite=None`);
     res.send({
         logout : false,
         login : true
